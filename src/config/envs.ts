5a6cb2db5a767c5f095a0a6704739fbb7c57b8ad
Felipe <felipe.daniel.henriquez@gmail.com>
2025-01-01 20:39:30 -0300

    fix(config/envs): corregido sintaxis

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,7 +1,7 @@
-import { config } from "dotenv";
+import "dotenv/config";
 import {get} from "env-var";
 
 export const envs = {
-    PORT: get('PORT').required(true).asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').required(true).asString()
+    PORT: get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: get('PUBLIC_PATH').required().default('public').asString()
 }
