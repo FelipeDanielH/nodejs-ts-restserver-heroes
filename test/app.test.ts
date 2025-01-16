0253cc8a818fc9d814bfbdd22f5cd2d314bb3477
Felipe <felipe.daniel.henriquez@gmail.com>
2025-01-16 16:50:18 -0300

    test(App): agrega pruebas sobre app.ts

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,20 @@
+import { envs } from "../src/config/envs";
+import  Server  from "../src/presentation/server";
+
+jest.mock('../src/presentation/server')
+
 describe("app.ts", ()=>{
 
+    test("should call ", async ()=>{
+        const app = await import('../src/app');
 
-    test("should work", ()=>{
+        expect(Server).toHaveBeenCalledTimes(1);
+        expect(Server).toHaveBeenCalledWith({
+            port: envs.PORT,
+            public_path: envs.PUBLIC_PATH,
+            routes: expect.any( Function )
+        })
 
-        expect(true).toBeTruthy;
+        expect(Server.prototype.start).toHaveBeenCalled()
     })
 })
