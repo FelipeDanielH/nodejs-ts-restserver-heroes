2415358f3ebf735af6fe3651e75b422811a2700f
Felipe <felipe.daniel.henriquez@gmail.com>
2025-01-18 09:22:53 -0300

    test(routes): crea prueba funcional sobre la ruta GET de /api/todos

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { server } from "../../TestServer";
+import { prisma } from "../../../src/data/postgres";
 
 describe('presentation/todos/routes.ts', () => {
 
@@ -11,12 +12,29 @@ describe('presentation/todos/routes.ts', () => {
         server.close();
     })
 
-    test("should return TODO's", async () => {
+    beforeEach( async ()=> {
+        await prisma.todo.deleteMany();
+    })
 
-        const {body} = await request(server.app)
+    const todo1 = {text: 'mensaje de prueba'};
+    const todo2 = {text: 'mensaje de prueba 2'};
+
+    test("should return TODO's api/todos", async () => {
+
+        await prisma.todo.createMany({
+            data: [todo1,todo2]
+        })
+
+        const { body } = await request(server.app)
             .get('/api/todos')
             .expect(200)
 
-        console.log(body);
+        expect(body).toBeInstanceOf(Array);
+        expect(body.length).toBe(2);
+        expect(body[0].text).toEqual(todo1.text);
+        expect(body[1].text).toEqual(todo2.text);
+        expect(body[0].completedAt).toBeNull;
+
+
     })
 })
